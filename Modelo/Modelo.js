a3d5155bfada74e8ca6c8669e3f93ae0d7e2544d
menezessn <mar_chelo10@hotmail.com>
2021-03-26 21:50:59 -0300

    ZUERA

---
+++ Modelo/Modelo.js
@@ -0,0 +1,7 @@
+function testar(){
+    res = document.getElementById('res');
+    window.alert("Se os dados estiverem corretos, o resultado será enviado por e-mail em breve. Se cuide e fique em casa!");
+    res.innerHTML = "<p>Obrigado por usar nosso site. Se os dados estiverem corretos você irá receber um e-mail com o resultado, caso contrário, você pode corrigir os dados e fazer o teste novamente. Se cuide e se puder fique em casa.</p>"
+
+
+}
